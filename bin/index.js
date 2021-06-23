db0786f02770c14c59d86df6556d39479f5f7734
xuemengge <xuemengge@zhihu.com>
2021-06-23 22:38:04 +0800

    fix: add filter and eslint

--- bin/index.js
+++ bin/index.js
@@ -1,49 +1,5 @@
-#!/usr/bin/env node
-const getResult = require("../lib/index")
-const findExactFile = require("../lib/findExactFile")
-const root = process.cwd()
+const filterRepo = require('../lib/index')
 
-// TODO: remove addIgnore
-// if (process.argv[2] == "--ignore") {
-//   for (var i = 3; i < process.argv.length; i++) {
-//     addIgnore.push(process.argv[i])
-//   }
-// }
+const targetPath = '/Users/xuemengge/zhihu/clone3'
 
-// getResult(
-//   "/Users/xuemengge/zhihu/heifetz/src/pages/campaign",
-//   [],
-//   "/Users/xuemengge/playground/find-js-dependencies/campaign.json"
-// )
-
-// getResult(
-//   "/Users/xuemengge/zhihu/heifetz/src/server.js",
-//   [],
-//   "/Users/xuemengge/playground/find-js-dependencies/server.json"
-// )
-
-getResult(
-  "/Users/xuemengge/zhihu/heifetz/src/apps/sem",
-  [],
-  "/Users/xuemengge/playground/find-js-dependencies/sem.json"
-)
-
-// getResult(
-//   "/Users/xuemengge/zhihu/heifetz/src/components/Error/ErrorPage/index.js",
-//   [],
-//   "/Users/xuemengge/playground/find-js-dependencies/errorpage1.json"
-// )
-
-// getResult(
-//   "/Users/xuemengge/zhihu/heifetz/src/routes/index.js",
-//   [],
-//   "/Users/xuemengge/playground/find-js-dependencies/r.json"
-// )
-
-// getResult(
-//   "/Users/xuemengge/zhihu/heifetz/src/components/AppHeader/MessagesLoader.js",
-//   [],
-//   "/Users/xuemengge/playground/find-js-dependencies/ml.json"
-// )
-
-//findExactFile("/Users/xuemengge/zhihu/heifetz/src/routes/routes")
+filterRepo(targetPath, 'src/apps/sem')
